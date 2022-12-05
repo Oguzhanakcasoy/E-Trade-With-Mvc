28b5f729cc00a24847ed5606c22c58871ab7f9b6
Mehmet Oğuzhan Akçasoy <58490289+Oguzhanakcasoy@users.noreply.github.com>
2022-12-05 14:43:14 +0300

    E-Trade-Proje

---
+++ E-TradeProje/Entity/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+    namespace E_TradeProje.Entity
+{
+    public class DataContext:DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category>Categories { get; set; }
+    }
+}
